1afaa68c4300871ce54bc707bdf1a1b1da58200f
hyuk <tig05205@naver.com>
2024-08-22 22:29:07 +0900

    post

--- src/main/java/com/example/hello/controller/ApiController.java
+++ src/main/java/com/example/hello/controller/ApiController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api1")
 public class ApiController {
 
     @GetMapping("/hello")
