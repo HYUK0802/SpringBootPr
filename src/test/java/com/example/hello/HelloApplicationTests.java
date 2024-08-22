8f6ef405b75c525c777a1d7def722cc451f0cb3f
hyuk <tig05205@naver.com>
2024-08-23 00:28:23 +0900

    Delete

--- src/test/java/com/example/hello/HelloApplicationTests.java
+++ src/test/java/com/example/hello/HelloApplicationTests.java
@@ -1,5 +1,6 @@
 package com.example.hello;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +9,8 @@ class HelloApplicationTests {
 
 	@Test
 	void contextLoads() {
+
+		var objectMapper = new ObjectMapper();
 	}
 
 }
